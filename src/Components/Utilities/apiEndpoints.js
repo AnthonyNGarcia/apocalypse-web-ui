0e3e0f0aa4caf415d4f6d40e5e0d7ebc6e9cb458
AnthonyNGarcia <lifeandsavior@hotmail.com>
2021-06-14 11:06:31 -0700

    updated update to reflect new domain name for api nlb

--- src/Components/Utilities/apiEndpoints.js
+++ src/Components/Utilities/apiEndpoints.js
@@ -4,7 +4,7 @@
  * urls for components to start making calls, to reduce redundancy and the
  * associated possibilities for typos/errors.
  */
-const apiBasePath = 'https://apocalypse-testing-nlb-d7c60e558daeb02d.elb.us-west-1.amazonaws.com:9595';
+const apiBasePath = 'https://apocalypse-dev-api-nlb-337c332a0c60c639.elb.us-west-1.amazonaws.com:9595';
 
 const apiEndpoints = {
   basePath: apiBasePath,
