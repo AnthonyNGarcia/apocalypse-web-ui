bbb2fcd54aa18095a2d31bcf66202388acbc867a
AnthonyNGarcia <lifeandsavior@hotmail.com>
2022-11-16 23:23:30 -0800

    Updating apiBasePath to include port, aiming for single NLB for both frontend/backend, and cant have both on default port 443

--- src/Components/Utilities/apiEndpoints.js
+++ src/Components/Utilities/apiEndpoints.js
@@ -4,8 +4,10 @@
  * urls for components to start making calls, to reduce redundancy and the
  * associated possibilities for typos/errors.
  */
-const apiBasePath = 'https://dev-api.playapocalypse.com';
+
 // const apiBasePath = 'http://localhost:9595';
+// const apiBasePath = 'http://54.71.95.128:9595';
+const apiBasePath = 'https://dev-api.playapocalypse.com:9595';
 
 const apiEndpoints = {
   basePath: apiBasePath,
