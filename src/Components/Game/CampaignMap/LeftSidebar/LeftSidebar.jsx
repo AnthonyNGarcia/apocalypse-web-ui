489bb4e6ab31d5c556f0dd7ecc486245b3f8e832
AnthonyNGarcia <lifeandsavior@hotmail.com>
2022-12-18 15:26:28 -0800

    Added forfet/save game buttons on campaign map, and some clearer unit/city borders

--- src/Components/Game/CampaignMap/LeftSidebar/LeftSidebar.jsx
+++ src/Components/Game/CampaignMap/LeftSidebar/LeftSidebar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import './LeftSidebar.css';
 import PLAYER from '../../../Utilities/playerEnums';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import apiEndpoints from '../../../Utilities/apiEndpoints';
 
 /**
  *
@@ -13,9 +17,48 @@ import PLAYER from '../../../Utilities/playerEnums';
  * @return {JSX} to render
  */
 const LeftSidebar = (props) => {
+  const forfeitGameHandler = (e) => {
+    e.preventDefault();
+    const leaveGameRequest = {
+      gameId: props.gameId,
+      inGamePlayer: {
+        userId: props.ownUserId,
+      },
+    };
+    axios.patch(apiEndpoints.gameController + '/leave', leaveGameRequest);
+  };
+
+  const saveGameHandler = async (e) => {
+    e.preventDefault();
+    const response = await axios.get(apiEndpoints.gameController + '/game-save/' + props.gameId);
+    const newSavedGames = {...props.savedGames};
+    newSavedGames[props.gameId] = response.data.gameData;
+    localStorage.setItem('SAVED_GAMES', JSON.stringify(newSavedGames));
+  };
+
   return (
     <div className='left-sidebar-sizer'>
       <div className='astridium-count-container'>
+        <Row noGutters className='left-sidebar-menu-buttons'>
+          <Col md={6}>
+            <Button
+              variant='danger'
+              disabled={!props.isOwnTurn}
+              onClick={forfeitGameHandler}
+            >
+            Forfeit Game
+            </Button>
+          </Col>
+          <Col md={6}>
+            <Button
+              variant='primary'
+              disabled={!props.isOwnTurn}
+              onClick={saveGameHandler}
+            >
+            Save Game
+            </Button>
+          </Col>
+        </Row>
         <Row noGutters>
           <p>Astridium Collected: {
             props.ownPlayerData.astridiumCollected}</p>
@@ -61,14 +104,23 @@ const mapStateToProps = (state) => {
     ownPlayerData: state.gamePlayer.ownPlayerNumber === PLAYER.ONE ?
       state.gamePlayer.playerOne : state.gamePlayer.playerTwo,
     ownPlayerNumber: state.gamePlayer.ownPlayerNumber,
+    isOwnTurn: state.gamePlayer.ownPlayerNumber ===
+      state.gamePlayer.playerWhoseTurnItIs,
+    gameId: state.game.gameId,
+    ownUserId: state.general.ownUserId,
+    savedGames: state.general.savedGames,
   };
 };
 
 LeftSidebar.propTypes = {
-  playerOne: PropTypes.any,
-  playerTwo: PropTypes.any,
-  ownPlayerData: PropTypes.any,
-  ownPlayerNumber: PropTypes.any,
+  playerOne: PropTypes.object,
+  playerTwo: PropTypes.object,
+  ownPlayerData: PropTypes.object,
+  ownPlayerNumber: PropTypes.string,
+  isOwnTurn: PropTypes.bool,
+  gameId: PropTypes.string,
+  ownUserId: PropTypes.string,
+  savedGames: PropTypes.object,
 };
 
 export default connect(mapStateToProps)(LeftSidebar);
